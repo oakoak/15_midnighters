2a98c6f5eb29ce56612178887c60dd06f926547b
varan <varan@VARAN-PC>
2018-07-25 22:07:09 +0300

    add except

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,22 +1,26 @@
 import requests
+import pytz
 import datetime
 
 
-def load_attempts():
-    response = requests.get(
-        "https://devman.org/api/challenges/solution_attempts/?page=2"
-    )
-    for page in response.json()["records"]:
-
+def load_attempts(dev_url):
+    response = requests.get(dev_url).json()
+    for page in response["records"]:
         yield page
 
 
-def get_midnighters():
+def get_local_user_time(attempt):
+    time_zone = pytz.timezone(attempt['timezone'])
+    return datetime.datetime.fromtimestamp(attempt['timestamp'], time_zone)
+
+
+def get_midnighters(attempt, time_from, time_to):
     midnighters = []
-    for user in load_attempts():
-        if datetime.datetime.utcfromtimestamp(user["timestamp"]).hour <= 6 \
-                and user["username"] not in midnighters:
-            midnighters.append(user["username"])
+    for user in attempt:
+        user_name = user["username"]
+        if time_from <= get_local_user_time(user).hour <= time_to \
+                and user_name not in midnighters:
+            midnighters.append(user_name)
     return midnighters
 
 
@@ -26,5 +30,14 @@ def print_midnighters(midnighters):
 
 
 if __name__ == '__main__':
-    midnighters = get_midnighters()
+    dev_url = "https://devman.org/api/challenges/solution_attempts/?page=2"
+    time_from = 0
+    time_to = 6
+
+    try:
+        attempt = load_attempts(dev_url)
+        midnighters = get_midnighters(attempt, time_from, time_to)
+    except requests.HTTPError as error:
+        exit("Error: {}".format(error))
+
     print_midnighters(midnighters)
